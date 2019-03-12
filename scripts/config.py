e8d495e901f6cc6d5d859287b0b6c0321d3303f9
patr0nus <dk4rest@gmail.com>
2019-03-12 21:07:36 +0800

    Try build and release Linux version

--- scripts/config.py
+++ scripts/config.py
@@ -2,7 +2,7 @@ assert __name__ != "__main__"
 
 import os
 
-nodeVersion = "v11.11.0"
+nodeVersion = os.environ['LIBNODE_NODE_VERSION']
 configFlags = os.environ.get('LIBNODE_CONFIG_FLAGS')
 x86 = os.environ.get('LIBNODE_X86') == '1'
 zipBasenameSuffix = os.environ.get('LIBNODE_ZIP_SUFFIX', '')

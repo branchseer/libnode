d846d0307159954642848416e7a3bfe9a9856718
patr0nus <dk4rest@gmail.com>
2019-03-12 15:38:08 +0800

    Allowing config_flags to be None

--- scripts/build.py
+++ scripts/build.py
@@ -12,11 +12,18 @@ if sys.platform == 'win32':
     vcbuildArgs = [ 'static', 'openssl-no-asm' ]
     if config.x86:
         vcbuildArgs.append('x86')
+
+    env = os.environ.copy()
+    if config.configFlags is not None:
+        env['config_flags'] = config.configFlags
     
     subprocess.check_call(
         ['cmd', '/c', 'vcbuild.bat'] + vcbuildArgs,
-        env={ **os.environ, 'config_flags': config.configFlags }
+        env=env
     )
 else:
-    subprocess.check_call(['./configure', '--enable-static'] + [config.configFlags])
+    configureArgvs = ['./configure', '--enable-static']
+    if config.configFlags is not None:
+        configureArgvs.append(config.configFlags)
+    subprocess.check_call(configureArgvs)
     subprocess.check_call(['make', '-j{}'.format(os.cpu_count())])
--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,6 @@ assert __name__ != "__main__"
 import os
 
 nodeVersion = "v11.11.0"
-configFlags = os.environ.get('libnode_config_flags', '')
+configFlags = os.environ.get('libnode_config_flags')
 x86 = os.environ.get('libnode_x86') == '1'
 zipBasenameSuffix = os.environ.get('libnode_zip_suffix', '')

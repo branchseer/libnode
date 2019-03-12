c5906f397314239b41a8e2483d24474380b82fe3
patr0nus <dk4rest@gmail.com>
2019-03-12 15:01:05 +0800

    Check the value of libnode_x86

--- scripts/config.py
+++ scripts/config.py
@@ -4,5 +4,5 @@ import os
 
 nodeVersion = "v11.11.0"
 configFlags = os.environ.get('libnode_config_flags', '')
-x86 = os.environ.get('libnode_x86') != None
+x86 = os.environ.get('libnode_x86') == '1'
 zipBasenameSuffix = os.environ.get('libnode_zip_suffix', '')

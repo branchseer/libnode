b979f1e1352aece92cece7014733015ee24ed77c
patr0nus <dk4rest@gmail.com>
2019-11-10 14:02:57 +0800

    Make configFlags a list

--- scripts/build.py
+++ scripts/build.py
@@ -23,8 +23,7 @@ if sys.platform == 'win32':
         vcbuildArgs.append('x86')
 
     env = os.environ.copy()
-    if config.configFlags is not None:
-        env['config_flags'] = config.configFlags
+    env['config_flags'] = ' '.join(config.configFlags)
     
     subprocess.check_call(
         ['cmd', '/c', 'vcbuild.bat'] + vcbuildArgs,
@@ -33,6 +32,6 @@ if sys.platform == 'win32':
 else:
     configureArgvs = ['./configure', '--enable-static']
     if config.configFlags is not None:
-        configureArgvs.append(config.configFlags)
+        configureArgvs += config.configFlags
     subprocess.check_call(configureArgvs)
     subprocess.check_call(['make', '-j{}'.format(os.cpu_count())])
--- scripts/config.py
+++ scripts/config.py
@@ -3,6 +3,6 @@ assert __name__ != "__main__"
 import os
 
 nodeVersion = os.environ['LIBNODE_NODE_VERSION']
-configFlags = os.environ.get('LIBNODE_CONFIG_FLAGS')
+configFlags = [] if ('LIBNODE_CONFIG_FLAGS' not in os.environ) else os.environ['LIBNODE_CONFIG_FLAGS'].split()
 x86 = os.environ.get('LIBNODE_X86') == '1'
 zipBasenameSuffix = os.environ.get('LIBNODE_ZIP_SUFFIX', '')

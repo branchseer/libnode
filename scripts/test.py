117a76063920ef7e2056061b0a4c7d1cee17a879
patr0nus <dk4rest@gmail.com>
2019-11-11 14:48:51 +0800

    Add the test script

---
+++ scripts/test.py
@@ -0,0 +1,24 @@
+assert __name__ == "__main__"
+
+import sys
+import os
+import subprocess
+import shutil
+
+from . import config
+
+os.chdir('test')
+shutil.rmtree('build', ignore_errors=True)
+os.mkdir('build')
+os.chdir('build')
+if sys.platform == 'win32':
+    subprocess.check_call(['cmake', '-G', 'Visual Studio 15 2017' + ('' if config.x86 else ' Win64'), '-S', '..'])
+else:
+    subprocess.check_call(['cmake', '-G', 'Unix Makefiles', '-DCMAKE_BUILD_TYPE=Release', '-S', '..'])
+
+subprocess.check_call(['cmake', '--build', '.', '--config', 'Release'])
+
+
+exec_path = 'Release\\libnode_test.exe' if sys.platform == 'win32' else './libnode_test'
+output_version = subprocess.check_output([exec_path, '-e', 'console.log(process.version)']).decode()
+assert output_version.strip() == config.nodeVersion

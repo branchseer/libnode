a30e105aec8dda24e58781944c6b8a6f3e993dea
patr0nus <dk4rest@gmail.com>
2020-03-04 18:57:15 +0800

    Generate vs 2017 project

--- scripts/build.py
+++ scripts/build.py
@@ -11,7 +11,7 @@ shutil.rmtree('build', ignore_errors=True)
 os.mkdir('build')
 os.chdir('build')
 if sys.platform == 'win32':
-    subprocess.check_call(['cmake', '-G', 'Visual Studio 16 2019', '-A', ('Win32' if config.x86 else 'x64'), '-S', '../src'])
+    subprocess.check_call(['cmake', '-G', 'Visual Studio 15 2017', '-A', ('Win32' if config.x86 else 'x64'), '-S', '../src'])
 else:
     subprocess.check_call(['cmake', '-G', 'Unix Makefiles', '-DCMAKE_BUILD_TYPE=Release', '-S', '../src'])
 os.chdir('..')

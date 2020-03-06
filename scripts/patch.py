dcc5f2b1baffef91df470add589c0f1ddc9ec5b3
chiwang <chiwang@microstrategy.com>
2020-03-06 15:34:39 +0800

    Revert patch.py changes

--- scripts/patch.py
+++ scripts/patch.py
@@ -6,12 +6,7 @@ import shutil
 
 from . import config
 
-baseDir = os.getcwd() + '/node-{}'.format(config.nodeVersion) 
-subprocess.check_call(['patch', '-d', baseDir, '-p1', '-i', '../uv.patch'])
+os.chdir('node-{}'.format(config.nodeVersion))
 
-
-print(os.listdir(baseDir))
-print(os.listdir(baseDir + '/include/node'))
-
-
-shutil.copyfile(baseDir + '/deps/uv/include/uv.h', baseDir + '/include/node/uv.h')
+subprocess.check_call(['patch', '-p1', '-i', '../uv.patch'])
+shutil.copyfile('deps/uv/include/uv.h', 'include/node/uv.h')

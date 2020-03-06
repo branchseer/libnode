c8377afad10cacfefe0e001605ffc32842d38a61
chiwang <chiwang@microstrategy.com>
2020-03-06 15:28:12 +0800

    List dir when patch

--- scripts/patch.py
+++ scripts/patch.py
@@ -8,4 +8,10 @@ from . import config
 
 baseDir = os.getcwd() + '/node-{}'.format(config.nodeVersion) 
 subprocess.check_call(['patch', '-d', baseDir, '-p1', '-i', '../uv.patch'])
+
+
+print(os.listdir(baseDir))
+print(os.listdir(baseDir + '/include/node'))
+
+
 shutil.copyfile(baseDir + '/deps/uv/include/uv.h', baseDir + '/include/node/uv.h')

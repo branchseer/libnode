7633e672fbf1d7a0841ee7580f0e46d2042c9e12
chiwang <chiwang@microstrategy.com>
2020-03-06 15:08:11 +0800

    copy -> copyfile

--- scripts/patch.py
+++ scripts/patch.py
@@ -9,4 +9,4 @@ from . import config
 os.chdir('node-{}'.format(config.nodeVersion))
 
 subprocess.check_call(['patch', '-p1', '-i', '../uv.patch'])
-shutil.copy('deps/uv/include/uv.h', 'include/node/uv.h')
+shutil.copyfile('deps/uv/include/uv.h', 'include/node/uv.h')

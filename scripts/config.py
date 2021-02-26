3270b6ea1d2703e7e614e998e0dfba3489897847
patr0nus <dk4rest@gmail.com>
2021-02-27 00:44:12 +0800

    typo

--- scripts/config.py
+++ scripts/config.py
@@ -2,7 +2,7 @@ assert __name__ != "__main__"
 
 import os
 
-nodeVersion = os.environ.get['LIBNODE_NODE_VERSION']
+nodeVersion = os.environ['LIBNODE_NODE_VERSION']
 configFlags = (os.environ.get('LIBNODE_CONFIG_FLAGS') or '').split()
 x86 = os.environ.get('LIBNODE_X86') == '1'
 zipBasenameSuffix = os.environ.get('LIBNODE_ZIP_SUFFIX', '')

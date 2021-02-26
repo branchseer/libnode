4738ec0e5bbaaae22f09384dd981219839730874
patr0nus <dk4rest@gmail.com>
2021-02-26 23:41:43 +0800

    Setup Github action

--- scripts/config.py
+++ scripts/config.py
@@ -2,7 +2,11 @@ assert __name__ != "__main__"
 
 import os
 
-nodeVersion = os.environ['LIBNODE_NODE_VERSION']
+nodeVersion = os.environ.get['LIBNODE_NODE_VERSION']
 configFlags = (os.environ.get('LIBNODE_CONFIG_FLAGS') or '').split()
 x86 = os.environ.get('LIBNODE_X86') == '1'
 zipBasenameSuffix = os.environ.get('LIBNODE_ZIP_SUFFIX', '')
+
+if os.environ.get('LIBNODE_NO_INTL', '') == '1':
+	configFlags += ['--without-intl']
+	zipBasenameSuffix += ['-nointl']

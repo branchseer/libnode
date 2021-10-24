57bc82e202f66df3518a6a71b353c03bed409c7d
patr0nus <patr0nus>
2021-10-24 15:18:47 +0800

    Use shutil.copytree

--- scripts/patch.py
+++ scripts/patch.py
@@ -1,9 +1,9 @@
 from . import config
 import os
 import subprocess
-from distutils.dir_util import copy_tree
+import shutil
 
 os.chdir('node-{}'.format(config.nodeVersion))
 
-copy_tree('../patch/node', '.')
+shutil.copytree('../patch/node', '.', dirs_exist_ok=True)
 subprocess.check_call(['patch', '-p1', '-i', '../patch/node.patch'])
